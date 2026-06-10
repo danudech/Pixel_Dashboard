a647e199280ac58323a8706e541ea17c71fdacd5
iDanudech <dany.yotali@hotmail.com>
2026-06-10 15:34:16 +0700

    image create

--- src/components/RoomScene.tsx
+++ src/components/RoomScene.tsx
@@ -1,7 +1,7 @@
 export function RoomScene() {
   return (
     <div className="roomArtwork">
-      <img className="roomBackdrop" src="/room.gif?v=1" alt="Pixel office room" />
+      <img className="roomBackdrop" src="/Clear_Room.gif?v=1" alt="Pixel office room" />
       {/* <img className="roomTable" src="/table.png?v=1" alt="" aria-hidden="true" /> */}
     </div>
   );
